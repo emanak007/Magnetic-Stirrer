f0f95f4f5efc085ec6531772f07753903c3da75b
Kaname <kaname@live.co.uk>
2026-03-20 19:16:12 +0000

    Merged original code with Jonah's menu logic.
     - Scrolling
     - Edit values of settings

--- Arduino/Magnetic_Stirrer/Magnetic_Stirrer.ino
+++ Arduino/Magnetic_Stirrer/Magnetic_Stirrer.ino
@@ -4,41 +4,136 @@
 #include <SPI.h>
 #include <RotaryEncoder.h>
 
-enum {MENU1, IDLE, SETTINGS, STIRRING}; 
-enum {EXPLICIT, IMPLICIT};
-int overall_state = MENU1;
-int control_mode_state = IMPLICIT;
+enum {IDLE, SETTINGS, STIRRING}; 
+int overall_state = IDLE;
 
 struct Inputs {
-  int knobDir; //1 clockwise 0 idle -1 anticlockwise
-  int buttonState; //maybe 1 for pressed, 2 for held, 0 for idle
+  int knobDir;     // 1 clockwise, 0 idle, -1 anticlockwise
+  int buttonState; // 1 pressed, 2 held, 0 idle
+} inputs;
+
+// Settings Screen Enum
+enum SettingsScreen {
+    SET_SPEED, SET_TEMP, SET_RAMP, SET_DURATION,
+    SET_TEMP_STOP, SET_MASS_STOP, SET_CLAMP, SET_MODE, CONFIRM,
+    NUM_SETTINGS 
 };
 
-Inputs inputs;
+struct SettingsData {
+    uint16_t target_rpm;
+    uint16_t target_temp;
+    uint16_t ramp_time_ms;
+    uint32_t stir_duration_ms;
+    uint32_t stir_till_temp;
+    uint32_t stir_till_mass;
+    bool clamp_enabled;
+    bool implicit_mode;
+} settings;
+
+int highlighted = SET_SPEED; // Start at index 0
+bool selected = false;
+
+const char* setting_names[NUM_SETTINGS] = { 
+    "SET SPEED", "SET TEMP", "SET RAMP", "SET DURATION",
+    "SET TEMP STOP", "SET MASS STOP", "SET CLAMP", "SET MODE", "CONFIRM"
+};
+
+
+void increment_setting(int h) {
+  if (h == SET_SPEED) settings.target_rpm += 10;
+  else if (h == SET_TEMP) settings.target_temp += 5;
+  else if (h == SET_RAMP) settings.ramp_time_ms += 1000;
+  else if (h == SET_DURATION) settings.stir_duration_ms += 5000;
+  else if (h == SET_TEMP_STOP) settings.stir_till_temp += 5;
+  else if (h == SET_MASS_STOP) settings.stir_till_mass += 10;
+}
+
+void decrement_setting(int h) {
+  if (h == SET_SPEED && settings.target_rpm >= 10) settings.target_rpm -= 10;
+  else if (h == SET_TEMP && settings.target_temp >= 5) settings.target_temp -= 5;
+  else if (h == SET_RAMP && settings.ramp_time_ms >= 1000) settings.ramp_time_ms -= 1000;
+  else if (h == SET_DURATION && settings.stir_duration_ms >= 5000) settings.stir_duration_ms -= 5000;
+  else if (h == SET_TEMP_STOP && settings.stir_till_temp >= 5) settings.stir_till_temp -= 5;
+  else if (h == SET_MASS_STOP && settings.stir_till_mass >= 10) settings.stir_till_mass -= 10;
+}
+
+void toggle_setting(int h) {
+  if (h == SET_CLAMP) settings.clamp_enabled = !settings.clamp_enabled;
+  else if (h == SET_MODE) settings.implicit_mode = !settings.implicit_mode;
+}
+
+void increment_highlighted() { if (highlighted < NUM_SETTINGS - 1) highlighted++; }
+
+void decrement_highlighted() { if (highlighted > 0) highlighted--; }
+
+bool Has_awoken() { return inputs.buttonState == 1 || inputs.buttonState == 2; }
+
+bool Has_started() { return highlighted == CONFIRM && inputs.buttonState == 1; }
+
+void Implicit_stirring_logic() {}
+
+void Explicit_stirring_logic() {}
+
+Inputs GetInputs() {
+  Inputs values;
+  values.buttonState = GetButtonState(); 
+  values.knobDir = GetRotaryDir();       
+  return values;
+}
+
+
+void Update_settings() {
+    int buttonPressed = inputs.buttonState;
+    int knobDir = inputs.knobDir;
+    
+    
+    if (selected) {
+        if (knobDir == -1) decrement_setting(highlighted);
+        if (knobDir == 1)  increment_setting(highlighted);
+    } else {
+        if (knobDir == 1)  increment_highlighted(); // Scroll down
+        if (knobDir == -1) decrement_highlighted(); // Scroll up
+    }
+
+    if (buttonPressed == 1) {
+        if (highlighted < SET_CLAMP) { 
+            selected = !selected; 
+        } else { 
+            toggle_setting(highlighted); 
+        }
+    }
+}
+
+// --- 4. ARDUINO SETUP & LOOP ---
 void setup() {
-
   Serial.begin(115200);
-  InitialiseInputs();
-  //setupLoad();
-  //IntialiseTempSensor();
-  //InitialiseDCMotor();
-  //InitialiseStepperMotor();
   InitialiseOLED();
-
+  InitialiseInputs();
+  
+  overall_state = SETTINGS;
 }
 
 void loop() {
   inputs = GetInputs();
+  //Serial.println(inputs.buttonState);
   switch (overall_state) {
-    case MENU1:
-      overall_state = UI(MENU1, inputs);
+    case IDLE:
+      if (Has_awoken()) { overall_state = SETTINGS; }
+      break;
+      
+    case SETTINGS: 
+      Update_settings();    
+      Display_settings();
+      if (Has_started()) { overall_state = STIRRING; }
+      break;
+      
+    case STIRRING:
+      if (settings.implicit_mode) {
+          Implicit_stirring_logic();
+      } else {
+          Explicit_stirring_logic();
+      }
+      break;
   }
-}
-
-Inputs GetInputs() {
-  Inputs values;
-  values.buttonState = GetButtonState();
-  values.knobDir = GetRotaryDir();
-  return values;
 }
 
--- Arduino/Magnetic_Stirrer/UserInterface.ino
+++ Arduino/Magnetic_Stirrer/UserInterface.ino
@@ -1,46 +1,58 @@
-U8G2_SSD1362_256X64_1_4W_HW_SPI u8g2(U8G2_R0,  /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
-static unsigned int cursorPos = 1;
-int lineSpacing = 15;
+U8G2_SSD1362_256X64_1_4W_HW_SPI u8g2(U8G2_R0, /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
 
 void InitialiseOLED() {
-  u8g2.begin(); // Start the display
+    u8g2.begin(); 
 }
 
-int UI (int MENU, Inputs input) {
-  int menuLimit = GetMenuLimit(MENU);
-  cursorPos += MoveCursor(menuLimit, input.knobDir, cursorPos);
-  u8g2.firstPage();
-  do {
-    DisplayOptions(MENU);
-    DisplayCursor(cursorPos);
-  } while (u8g2.nextPage());
-  return MENU;
+
+void Display_setting(int idx, int row) { 
+    int y_pixel = (row + 1) * 20; 
+    
+    u8g2.setCursor(10, y_pixel); 
+    
+    if (row == 1) {
+        if (selected) {
+            u8g2.print("* "); 
+        } else {
+            u8g2.print("> "); 
+        }
+    } else {
+        u8g2.print("  "); 
+    }
+
+    u8g2.print(setting_names[idx]);
+
+    if (idx != CONFIRM) {
+        u8g2.print(": ");
+    }
+
+    if (idx == SET_SPEED) u8g2.print(settings.target_rpm);
+    else if (idx == SET_TEMP) u8g2.print(settings.target_temp);
+    else if (idx == SET_RAMP) u8g2.print(settings.ramp_time_ms);
+    else if (idx == SET_DURATION) u8g2.print(settings.stir_duration_ms);
+    else if (idx == SET_TEMP_STOP) u8g2.print(settings.stir_till_temp);
+    else if (idx == SET_MASS_STOP) u8g2.print(settings.stir_till_mass);
+    else if (idx == SET_CLAMP) u8g2.print(settings.clamp_enabled ? "ON" : "OFF");
+    else if (idx == SET_MODE) u8g2.print(settings.implicit_mode ? "IMPLICIT" : "EXPLICIT");
 }
 
-int GetMenuLimit(int MENU) {
-  if (MENU == MENU1) return 3;
-  return 1;
-}
 
-int MoveCursor(int limit, int knobDir, int pos) {
-  if ((knobDir == 1) && (pos < limit)) {
-    return 1; //move down
-  }
-  else if ((knobDir == -1) && (pos > 1)) {
-    return -1; //move up
-  }
-  return 0;
-}
+void Display_settings() {
 
-void DisplayCursor(int lineNum) {
-  u8g2.drawStr(0, lineNum * 15,">");
-}
+    u8g2.firstPage();
+    do {
+        u8g2.setFont(u8g2_font_ncenB10_tr); 
+        
+        // Top (-1), Middle (0), Bottom (1)
+        for (int i = -1; i <= 1; i++) {
+            int idx = highlighted + i;
+          
 
-void DisplayOptions(int MENU) {
-  if (MENU == MENU1) {
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(15,15,"Start");
-    u8g2.drawStr(15,30,"Callibrate Load Sensor");
-    u8g2.drawStr(15,45,"Sensor Readings");
-  }
+            if (idx < 0 || idx >= NUM_SETTINGS) continue;
+            
+            int displayRow = i + 1; 
+            
+            Display_setting(idx, displayRow); 
+        }
+    } while (u8g2.nextPage());
 }
